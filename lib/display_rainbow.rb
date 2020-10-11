d9970583b44898640f5afd15eabb58f20fabcb2d
Tanesha Goodman <t24good@icloud.com>
2020-10-11 20:48:42 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
-# Write your #display_rainbow method here
-def display_rainbow(colors)
-  puts "Colors: #{R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet}"
+# Write your #display_rainbow method here display_rainbow(colors)
+color = display_rainbow(color)
+  puts "Color: #{R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet}"
 end
